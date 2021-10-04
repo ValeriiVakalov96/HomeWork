a1fb1896944ce493af4dd259bbfb8e6f040bba19
Valerii22 <valera.vakalov22@gmail.com>
2021-10-04 17:19:06 +0200

    completed HW1, HW2, HW3

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.userImage}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={s.chatBox}>
+                <div className={s.userName}><span>{props.name}</span></div>
+                <div className={s.msgTimeBox}>
+                    <div className={s.textMessage}><span>{props.message}</span></div>
+                    <div className={s.messageTime}><span>{props.time}</span></div>
+                </div>
+            </div>
         </div>
     )
 }
